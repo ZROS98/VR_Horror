08ce9f186f42e5600278415cb0d1a2f2b22809b9
Vladyslav Riabchuk <vladyslav.riabchuk@4experience.co>
2023-04-05 00:50:15 +0200

    Add reycast hitting objects

--- VR_Horror/Assets/Scripts/DEBUG_CharacterController.cs
+++ VR_Horror/Assets/Scripts/DEBUG_CharacterController.cs
@@ -26,6 +26,7 @@ public class DEBUG_CharacterController : MonoBehaviour
     {
         HandleMovement();
         HandleRotation();
+        HandleRaycast();
     }
 
     private void LockCursor ()
@@ -53,5 +54,25 @@ public class DEBUG_CharacterController : MonoBehaviour
 
         PlayerCamera.localRotation = Quaternion.Euler(RotationAxisX, 0f, 0f);
         Player.Rotate(Vector3.up * mouseX, Space.World);
+    }
+
+    private void HandleRaycast ()
+    {
+        RaycastHit hit;
+        float TheDistace;
+        
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+        
+        if (Physics.Raycast(transform.position, (forward), out hit))
+        {
+            TheDistace = hit.distance;
+            print(TheDistace + " " + hit.collider.gameObject.name);
+ 
+            // if (hit.collider.tag == "Object")
+            // {
+            //     hit.collider.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            // }
+        }
     }
 }
