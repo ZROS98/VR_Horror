468bc057f69ebdcb5cc7319cd24b0c959d658902
Vladyslav <zros98gmail.com>
2023-05-24 16:23:16 +0200

    Add collectables, change teleportation

---
+++ VR_Horror/Assets/Scripts/InteractiveCollectibles.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace VR_Horror
+{
+    public class InteractiveCollectibles : MonoBehaviour
+    {
+        public void HandleCollection()
+        {
+            Debug.Log($"{gameObject.name} was collected");
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ VR_Horror/Assets/Scripts/InteractiveCollectiblesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace VR_Horror
+{
+    public class InteractiveCollectiblesController : MonoBehaviour
+    {
+        [field: SerializeField]
+        private List<InteractiveCollectibles> InteractiveCollectiblesCollection { get; set; }
+
+        public void CollectObject( InteractiveCollectibles interactiveCollectibles)
+        {
+            interactiveCollectibles.HandleCollection();
+        }
+    }
+}
